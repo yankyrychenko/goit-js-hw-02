35f831877a96bb2e0d143837539e3ac581043d2c
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2020-12-08 20:23:06 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -10,10 +10,10 @@ function checkForSpam(str) {
   return false;
 }
 
-//console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('Latest technology news')); // false
 
-//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-//console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Get best sale offers now!')); // true
 
-//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
